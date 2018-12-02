77555de65ca51108cdfbb5699b1cf90305e0deac
ericgoodwin <edgbpc@mail.umsl.edu>
2018-12-02 13:35:28 -0600

    Initial Commit

---
+++ EricGoodwinPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EricGoodwinPart2
+//
+//  Created by Eric Goodwin on 12/2/18.
+//  Copyright © 2018 umsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
